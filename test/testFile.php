d9e5f69de20b6a1da4036e60643c6a49597e43db
Aleksandr <aleksandr.uteam@gmail.com>
2021-10-12 11:36:30 +0300

    Реализация тестов на основании классов и объектов

--- test/testFile.php
+++ test/testFile.php
@@ -3,20 +3,27 @@
 include_once "../config/config.php";
 include_once "../src/File.php";
 
-
-/**
- * Тестирование функции addUser.
- * Создание файла с id пользователя.
- * Запись в файл по id, информацию о пользователе.
- */
-function testFIle()
+class testFile
 {
-    addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-1900", "доп. инфа");
+    function __construct()
+    {
+    }
 
-    addUser("Александр", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-2010", "доп. инфа");
+    /**
+     * Тестирование функции addUser.
+     * Создание файла с id пользователя.
+     * Запись в файл по id, информацию о пользователе.
+     */
+    function testFIle()
+    {
+        addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-1900", "доп. инфа");
 
-    addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-2100", "доп. инфа");
+        addUser("Александр", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-2010", "доп. инфа");
+
+        addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-2100", "доп. инфа");
+    }
 }
 
+$testFile = new testFile();
 
-testFIle();
+$testFile->testFIle();
