0235bc9b7a4b1da820f3830344c81500666b9e7d
Aleksandr <aleksandr.uteam@gmail.com>
2021-10-12 10:26:42 +0300

    Корректировка тестов

--- test/testFile.php
+++ test/testFile.php
@@ -4,4 +4,27 @@ include_once "../config/config.php";
 include_once "../src/File.php";
 
 
-addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-1900", "доп. инфа");
+/**
+ * Тестирование функции addUser
+ * Создание файла с id пользователя
+ * Запись в файл по id информации о пользователе
+ */
+function testFIle()
+{
+    addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-1900", "доп. инфа");
+
+    addUser("Александр", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-2010", "доп. инфа");
+
+    addUser("Aleksandr", "password", "admin@localsite.ru", "Гребенников Александр Сергеевич", "8-12-2100", "доп. инфа");
+}
+
+/**
+ * Тестирование Валидации полей
+ */
+function testValidation()
+{
+
+}
+
+
+testFIle();
