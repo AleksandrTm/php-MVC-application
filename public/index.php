24a9ace4a487c457fd7368d3976e7d76fb394e81
Aleksandr <aleksandr_dev@vk.com>
2021-10-14 16:42:29 +0300

    Выносим Routes из index.php в логику

--- public/index.php
+++ public/index.php
@@ -1,28 +1,6 @@
 <?php
 require_once "../src/Autoload.php";
 
-use Config\Paths;
+use src\Core\Routes;
 
-switch ($_SERVER['REQUEST_URI']) {
-    case "/user/id/delete":
-    case "/":
-        require_once Paths::DIR_VIEWS . "viewUsers.php";
-        break;
-    case "/registration":
-        require_once Paths::DIR_VIEWS . "registration.php";
-        break;
-    case "/login":
-        require_once Paths::DIR_VIEWS . "login.php";
-        break;
-    case "/user/add":
-        require_once Paths::DIR_VIEWS . "add-user.php";
-        break;
-//    case "/user/id/delete":
-//        require_once Paths::DIR_VIEWS . "/";
-//        break;
-    case "/user/if/edit":
-        require_once Paths::DIR_VIEWS . "edit-user.php";
-        break;
-    default:
-        require_once Paths::DIR_VIEWS . "page-404.php";
-}
+Routes::getInstance()->Run();
