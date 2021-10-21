360c562a834f82c75f5525fee28aa4e2c43ab00e
AleksandrTm <aleksandr.uteam@gmail.com>
2021-10-21 17:05:48 +0300

    - Убраны лишние проверки на null

--- views/main.php
+++ views/main.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $title ?? null ?></title>
+    <title><?= $title ?></title>
     <link rel="icon" href="/../img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="/../css/style.css">
 </head>
@@ -16,7 +16,7 @@
     <!-- Конец Шаблона: header -->
     <div class="container">
         <!-- Начала Шаблона: container -->
-        <?php require_once $template ?? null . '.php'; ?>
+        <?php include_once $template . '.php'; ?>
         <!-- Конец Шаблона: container -->
     </div>
     <!-- Начала Шаблона: footer -->
