10f39ea4011efa8d4a7b8b001f10eb17514150e7
AleksandrTm <aleksandr.uteam@gmail.com>
2021-10-22 09:38:37 +0300

    Добавлено меню и стили для него

--- views/template/header.php
+++ views/template/header.php
@@ -14,14 +14,11 @@
         </div>
     </div>
     <div class="menu">
-        <a href="/">Главная</a>
-        <span>|</span>
-        <a href="/">Новости</a>
-        <span>|</span>
-        <a href="/">Статьи</a>
+        <a class="menu-link" href="/">Главная</a>
+        <a class="menu-link" href="/">Новости</a>
+        <a class="menu-link" href="/">Статьи</a>
         <?php if (($_SESSION['role'] ?? null) != "guest") { ?>
-        <span>|</span>
-        <a href="/view/users">Пользователи</a>
+        <a class="menu-link" href="/view/users">Пользователи</a>
         <?php } ?>
     </div>
 </header>
